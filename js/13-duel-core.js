c4957a6a488310d04de6a4a22b1e4dd75aa9ee38
leronbida-web <leronbida@gmail.com>
2026-07-26 11:17:04 -0300

    gif arball
    ar ball

--- js/13-duel-core.js
+++ js/13-duel-core.js
@@ -636,6 +636,7 @@ const DUEL_ATTACK_SPRITES = {
     fogo: 'https://i.imgur.com/bNxh225.gif',
     agua: 'https://i.imgur.com/QNXld6b.gif',
     terra: 'https://i.imgur.com/HmFBX3N.png',
+    ar: 'https://i.imgur.com/GV73Bxo.gif'
   },
 };
 
